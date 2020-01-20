e1b688bd3cc2cca4168ffbbe6b23729ffcfde2fa
stanworld <songtancs@gmail.com>
2020-01-19 19:43:29 -0500

    cpp project template

---
+++ devsproject/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.hpp"
+
+int main() {
+    std::cout << "Hello Easy C++ project!" << std::endl;
+    test t;
+    t.yes();
+}
---
+++ devsproject/src/test.cpp
@@ -0,0 +1,10 @@
+#include "test.hpp"
+#include <iostream>
+
+using namespace std;
+
+test::test(){}
+
+void test::yes(){
+    cout<<"invoked"<<endl;
+}
